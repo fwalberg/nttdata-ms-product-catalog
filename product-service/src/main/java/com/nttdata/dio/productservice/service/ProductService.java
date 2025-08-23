d115809297c077dd321b7eb6eb82b347aa0d31ed
Walberg Santos <fwalbergsantos@outlook.com>
2025-08-23 17:33:24 -0300

    feat: implement ProductRepository and ProductService for product management

--- product-service/src/main/java/com/nttdata/dio/productservice/service/ProductService.java
+++ product-service/src/main/java/com/nttdata/dio/productservice/service/ProductService.java
@@ -1,4 +1,19 @@
 package com.nttdata.dio.productservice.service;
 
+import com.nttdata.dio.productservice.dto.ProductDTO;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public interface ProductService {
+    List<ProductDTO> findAll();
+    ProductDTO findById(Long id);
+    ProductDTO save(ProductDTO productDTO);
+    void deleteById(Long id);
+
+    ProductDTO findByName(String name);
+    List<ProductDTO> findByNameContaining(String namePart);
 }

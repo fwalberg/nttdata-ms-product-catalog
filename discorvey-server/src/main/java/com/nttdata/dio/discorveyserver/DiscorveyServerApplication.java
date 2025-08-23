c72d83958ec6827429dfc87e4d425c3939bc6261
Walberg Santos <fwalbergsantos@outlook.com>
2025-08-22 23:18:14 -0300

    created discovery-sever ms

---
+++ discorvey-server/src/main/java/com/nttdata/dio/discorveyserver/DiscorveyServerApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.dio.discorveyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscorveyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscorveyServerApplication.class, args);
+    }
+
+}
---
+++ discorvey-server/src/test/java/com/nttdata/dio/discorveyserver/DiscorveyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.dio.discorveyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscorveyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

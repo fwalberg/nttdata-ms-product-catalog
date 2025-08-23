2848203608bbe54b8360fb36fcfa8170ad23ebcd
Walberg Santos <fwalbergsantos@outlook.com>
2025-08-22 22:53:03 -0300

    created product-service

---
+++ product-service/src/test/java/com/nttdata/dio/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.dio.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
